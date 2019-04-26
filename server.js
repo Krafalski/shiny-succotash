8c78f801b80844a220ab79ec4c4a98231a088c06
Karolin Rafalski <kr2174@caa.columbia.edu>
2019-04-26 16:27:18 -0400

    see console log

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-console.log(process.env)
+console.log('this is process.env', process.env)
 
 app.get('/backend', (req, res) => {
   res.json({ works: 'yeah works woo'})
